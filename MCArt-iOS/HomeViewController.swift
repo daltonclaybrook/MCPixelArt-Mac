02c5c8bb37b741e09fc5c8f6ab8079808385c666
Dalton Claybrook <daltonclaybrook@gmail.com>
2017-04-14 12:52:18 -0500

    WIP - stubbing out the image picker logic on iOS

--- MCArt-iOS/HomeViewController.swift
+++ MCArt-iOS/HomeViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import MobileCoreServices
 
 class HomeViewController: UIViewController {
     var collectionController: HomeCollectionViewController!
@@ -20,6 +21,50 @@ class HomeViewController: UIViewController {
     //MARK: Actions
     
     @IBAction func addButtonPressed(_ sender: Any) {
-//        let sheet = UIAlertController(title: "", message: <#T##String?#>, preferredStyle: <#T##UIAlertControllerStyle#>)
+        let sheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        sheet.addAction(UIAlertAction(title: "Take a Photo", style: .default, handler: { (action) in
+            self.showPicker(withSourceType: .camera)
+        }))
+        sheet.addAction(UIAlertAction(title: "Choose an Image", style: .default, handler: { (action) in
+            self.showPicker(withSourceType: .savedPhotosAlbum)
+        }))
+        sheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        present(sheet, animated: true, completion: nil)
+    }
+    
+    //MARK: Private
+    
+    private func showPicker(withSourceType type: UIImagePickerControllerSourceType) {
+        guard UIImagePickerController.isSourceTypeAvailable(type) else {
+            showAlert(title: "Oops", message: "The selected image source type is unavailable")
+            return
+        }
+        
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = type
+        pickerController.mediaTypes = [kUTTypeImage as String]
+//        pickerController.allowsEditing = true
+        present(pickerController, animated: true, completion: nil)
+    }
+    
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+}
+
+extension HomeViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        dismiss(animated: true, completion: nil)
+        guard let image = info[UIImagePickerControllerOriginalImage] as? UIImage else { return }
+        
+        
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
     }
 }
